9975feb3efe636f258790a85128de8fc68798f48
tomoya-kwansei <tomomail1997@gmail.com>
2022-08-13 10:20:44 +0900

    Initial Commit

---
+++ Carvide/CarvideApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarvideApp.swift
+//  Carvide
+//
+//  Created by 番庄智也 on 2022/08/13.
+//
+
+import SwiftUI
+
+@main
+struct CarvideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
